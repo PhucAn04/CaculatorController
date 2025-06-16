c510d3d1f18165fac50851cb6972fbe62b7c3048
PhucAn04 <pham27282@gmail.com>
2025-06-16 11:34:43 +0700

    Fisrt Caculator controller

---
+++ CaculatorController/CaculatorController/Controllers/CaculatorController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CaculatorController.Controllers
+{
+    public class CaculatorController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Caculator(double a, double b, string op)
+        {
+            double result = 0;
+            switch (op)
+            {
+                case "+":
+                    result = a + b;
+                    break;
+                case "-":
+                    result = a - b;
+                    break;
+                case "*":
+                    result = a * b;
+                    break;
+                case "/":
+                    result = a / b;
+                    break;
+                case "%":
+                    result = a % b;
+                    break;
+                case "^":
+                    result = Math.Pow(a, b);
+                    break;
+            }
+
+            ViewBag.Result = result;
+            ViewBag.A = a;
+            ViewBag.B = b;
+            ViewBag.Op = op;
+
+            return View("Index");
+        }
+    }
+}
